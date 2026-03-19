a82811883b98bb97a895a616bbe65db94c68574d
scottneww1-web <scott.neww1@gmail.com>
2026-03-19 01:25:10 -0500

    Add styling wrapper around BlitzAIPage
    Wrap BlitzAIPage in a styled div for better layout.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import BlitzAIPage from './pages/BlitzAIPage'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+<React.StrictMode>
+  <div style={{ background: "#000", minHeight: "100vh", padding: "20px" }}>
     <BrowserRouter>
       <BlitzAIPage />
     </BrowserRouter>
-  </React.StrictMode>
-)
+  </div>
+</React.StrictMode>
